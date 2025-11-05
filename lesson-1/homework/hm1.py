09dfb03be936d74f971db60f393b11b5f9fc1ae1
Shakboz Nurmatov <shaxboznurmatov51@gmail.com>
2025-11-05 07:57:28 +0500

    Add tasks for square, circle, and basic arithmetic

---
+++ lesson-1/homework/hm1.py
@@ -0,0 +1,32 @@
+# TASK 1 
+side = float(input("Enter the side of the square: "))
+perimeter = 4 * side
+area = side ** 2
+print("Perimeter:", perimeter)
+print("Area:", area)
+
+# TASK 2
+d=14
+pi=3.14
+length=pi*d
+print("Length",length)
+
+# TASK 3
+a = float(input("Enter number a: "))
+b = float(input("Enter number b: "))
+mean = (a + b) / 2
+print("Mean:", mean)
+
+# TASK 4 
+a= float(input("Enter number a:"))
+b= float(input("Enter number b:"))
+sum=a+b
+product=a*b
+square_a=a**2
+square_b=b**2
+print("Sum:",sum)
+print("Product:",product)
+print("Square of a:",square_a)
+print("Square of b:",square_b)
+
+
